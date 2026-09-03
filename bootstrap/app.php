e6f0089b897681d998052e3063d2e2254e3b45d4
PradiptaPPLG <pradipta02032009@gmail.com>
2026-09-03 17:34:43 +0700

    feat: initialize application bootstrap with custom middleware and JSON exception handling

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,7 +12,9 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        $middleware->alias([
+            'rm' => \App\Http\Middleware\RmMiddleware::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         $exceptions->shouldRenderJsonWhen(
